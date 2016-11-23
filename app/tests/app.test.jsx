5153ed92c97c9ffba00e551335f8bbd96f7a69fd
Roy Scheffers <r.scheffers@gmail.com>
2016-11-23 14:49:24 -0500

    Add automatic tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,5 @@
+var expect = require('expect');
+
+it('should properly run tests', () => {
+  expect(1).toBe(1);
+});
---
+++ tests/app.test.jsx
@@ -0,0 +1,10 @@
+var expect = require('expect');
+
+describe('Application tests', () => {
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+  it('2nd test', () => {
+    expect(1).toBe(1);
+  });
+});
